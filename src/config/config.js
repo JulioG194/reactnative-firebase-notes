272c714a9dcc636b93406050ac2c6266602191d9
Julio <julio.gonzalez2014a@gmail.com>
2019-12-30 09:14:27 -0500

    Aplicacion dennotas con firebase y react  native culminada

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+export const DB_CONFIG = {
+    apiKey: "AIzaSyBGujDrD1r78dkjz9gvyR06M-d1Ia_9I10",
+    authDomain: "reactnative-firebase-notas.firebaseapp.com",
+    databaseURL: "https://reactnative-firebase-notas.firebaseio.com",
+    projectId: "reactnative-firebase-notas",
+    storageBucket: "reactnative-firebase-notas.appspot.com",
+    messagingSenderId: "1048013242294",
+    appId: "1:1048013242294:web:9c39e60db9ed97d4320b17"
+};
